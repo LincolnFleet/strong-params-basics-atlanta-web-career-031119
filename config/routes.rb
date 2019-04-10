fcada90ea529adf0606bc3b67101a0e966c5d262
github username <github email address>
2019-04-10 10:51:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  tests can die in a fire!
+  
+  resources :posts
 end
